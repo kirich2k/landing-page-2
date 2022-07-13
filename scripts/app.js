40a6e3154c2e92538984fe1db3b85ac2b429b707
Kirill <86837669+kirich2k@users.noreply.github.com>
2022-07-13 03:31:42 +0300

    BEM

--- scripts/app.js
+++ scripts/app.js
@@ -4,18 +4,18 @@ const menu = document.getElementById("menu");
 
 let openMenu = () => {
     if (
-        burger?.classList[1] !== "_active" &&
-        menu?.classList[1] === "_disabled" &&
-        menu?.classList[1] !== "_fixed"
+        burger?.classList[2] !== "-active" &&
+        menu?.classList[2] === "-disabled" &&
+        menu?.classList[1] !== "-fixed"
     ) {
-        burger.classList.add("_active");
-        menu.classList.remove("_disabled");
-        menu.classList.add("_active");
-        main.classList.add("_fixed");
+        burger.classList.add("-active");
+        menu.classList.remove("-disabled");
+        menu.classList.add("-active");
+        main.classList.add("-fixed");
     } else {
-        burger.classList.remove("_active");
-        menu.classList.remove("_active");
-        menu.classList.add("_disabled");
-        main.classList.remove("_fixed");
+        burger.classList.remove("-active");
+        menu.classList.remove("-active");
+        menu.classList.add("-disabled");
+        main.classList.remove("-fixed");
     }
 };
